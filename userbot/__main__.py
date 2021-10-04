8b1c3304137f0e3ec50ea58dc623f3628ab53579
lolyz <68340411+lolyz@users.noreply.github.com>
2021-10-04 18:20:05 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Tornado-Userbot ⚙️ V{BOT_VER} [🌀 BERHASIL DIAKTIFKAN! 🌀")
+    f"Si-Userbot ⚙️ V{BOT_VER} [🌀 BERHASIL DIAKTIFKAN! 🌀")
 
 
 if len(argv) not in (1, 3, 4):
